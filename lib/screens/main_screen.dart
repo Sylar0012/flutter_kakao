eea218045baf97cc237798189d667acc538ff5d3
sylar0012 <kmc42692@naver.com>
2022-11-11 09:53:13 +0900

    mainScreen 위젯 기본코드 완성

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,11 +9,46 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MainScreen"),
+      body: IndexedStack(
+        // 여러페이지로 이동하기 위해 사용. 인덱스가 스텍으로 쌓임
+        index: _selectedIndex, // 현재 인덱스 값을 나타내는 변수
+        children: [
+          Center(child: Text("IndexStack 1")),
+          Center(child: Text("IndexStack 2")),
+          Center(child: Text("IndexStack 3")),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          // 선택된 아이콘의 인덱스를 보여줌.
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: "",
+          ),
+        ],
       ),
     );
   }
